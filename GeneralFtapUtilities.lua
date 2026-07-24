70e07d14fb60ab6d0c4847f41fece7d92d480191
Salami <168672214+truman983@users.noreply.github.com>
2026-07-24 14:29:54 -0500

    Fix character head existence check in utility function

--- GeneralFtapUtilities.lua
+++ GeneralFtapUtilities.lua
@@ -261,7 +261,7 @@ function Utils.Aura(func: (ModelInRange: Model) -> ())
 
     return task.spawn(function()
         while task.wait(0.05) do
-            if not lp.Character or not lp.Character.Head then return end 
+            if not lp.Character or not lp.Character:FindFirstChild("Head") then return end 
              local seen = {}
             local PartsInRange = workspace:GetPartBoundsInRadius(lp.Character.Head.Position, 19, params)
             for i,v in ipairs(PartsInRange) do
