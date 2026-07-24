2682bd30de6c4295b639afaf14a219b06048e71c
Salami <168672214+truman983@users.noreply.github.com>
2026-07-24 11:40:06 -0500

    Refactor utilities for toy management and validation

--- GeneralFtapUtilities.lua
+++ GeneralFtapUtilities.lua
@@ -8,7 +8,7 @@ local charEvents: Folder = repStorage:WaitForChild("CharacterEvents")
 local PlrEvents: Folder = repStorage:WaitForChild("PlayerEvents")
 local grabEvents = game:GetService("ReplicatedStorage"):WaitForChild("GrabEvents", 1)
 
-local menuToys: RemoteEvent = repStorage:WaitForChild("MenuToys", 1)
+local menuToys: Folder = repStorage:WaitForChild("MenuToys", 1)
 
 local deleteToyRemote: RemoteEvent = menuToys:WaitForChild("DestroyToy", 1)
 local spawnToyEvent: RemoteFunction = menuToys:WaitForChild("SpawnToyRemoteFunction", 1)
@@ -21,11 +21,15 @@ local struggleRem: RemoteEvent = charEvents:FindFirstChild("Struggle")
 local lp = plrServ.LocalPlayer
 local LpMouse = lp:GetMouse()
 
+local ToyFolder = game.Workspace:FindFirstChild(lp.Name.."SpawnedInToys")
+
 local ownedParts = {}
 local Connections = {}
 local threads = {}
 
 local AgEnabled = false
+
+
 
  local ServerMessages = {
         Message = function(Msg: string)
@@ -44,21 +48,40 @@ local AgEnabled = false
 
  }
 
-local function ValidGrabbable(obj, doDistCheck: boolean)
+ 
+function GetClosestPartOfModel(Model: Model)
+    local Closest = {
+        DistCheck = math.huge;
+    }
+
+    for i,v in pairs(Model:GetChildren()) do
+        if v:IsA("BasePart") then
+            local Dist: number = (v.Position - lp.Character.Head.Position).Magnitude
+            if Dist < Closest.DistCheck then
+                Closest.DistCheck = Dist
+                Closest.Object = v
+            end 
+        end
+    end
+
+    if Closest.Object then
+        return Closest.Object
+    else
+        warn("no valid object was found")
+        return
+    end
+
+end
+
+local function ValidGrabbable(obj)
 
     local allowedCollisionGroups = {
     "Items",
     "Default",
-	"Players",
-	"PlotItems"
+	"Players"
 }
 
-    if table.find(allowedCollisionGroups, obj.CollisionGroup) and obj.Anchored == false  then
-       
-        if doDistCheck then
-           return (obj.Position - lp.Character.Head.Position).Magnitude < 20
-        end
-
+    if table.find(allowedCollisionGroups, obj.CollisionGroup) and obj.Anchored == false then
         return true
     else
         return false
@@ -83,13 +106,14 @@ end
 
 
 
+function Utils.OwnModel(Model: Model)
 
-function Utils.OwnPart(Part)
+    local closestPart = GetClosestPartOfModel(Model)
 
-    if ValidGrabbable(Part) then
-        netOwnTarget(Part)
+    if ValidGrabbable(closestPart) then
+        netOwnTarget(closestPart)
         task.wait()
-        dropTarget(Part)
+        dropTarget(closestPart)
     end
 
 end
@@ -98,12 +122,10 @@ function Utils.OwnMouseTarget()
 
     local targ = LpMouse.Target
 	if targ then
-	    if ValidGrabbable(targ, true) then
+	    if ValidGrabbable(targ) then
 	        netOwnTarget(targ)
 	        task.wait()
 	        dropTarget(targ)
-
-			return targ
 	    end
 	end
 end
@@ -169,13 +191,29 @@ function Utils.AntiGrab(Enabled: boolean)
 
 end
 
-function Utils.SpawnToy()
-
+function Utils.SpawnToy(ToyName: string, Position: Vector3, Orientation: CFrame?)
+    local orient = Orientation or CFrame.new(0,0,0)
+    task.spawn(function()
+        spawnToyEvent:InvokeServer(ToyName, orient, Position)
+    end)
 end
 
-function Utils.DeleteToy()
+function Utils.DeleteToy(ToyName: string)
+    deleteToyRemote:FireServer(ToyName)
+end
 
+function Utils.OnToySpawn(func: (spawnedToy: Model) -> ())
+    local returnConn
+    
+    returnConn = ToyFolder.ChildAdded:Connect(function(child: Instance)
+        if child:IsA("Model") then
+            func(child)
+        end
+    end)
+
+    Connections["LPSpawnObject"] = returnConn
+
+    return returnConn
 end
 
 return Utils
-
